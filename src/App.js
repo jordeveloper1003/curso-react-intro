94e8c051592f3a41b5fc3c907dc41f4712d270ae
jordeveloper1003 <jordy.10.garcia@gmail.com>
2023-06-25 02:48:00 -0300

    " Etiqueta h1 agregada "

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import logo from './platzi.webp';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
+        <TodoItem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,10 +18,20 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoItem () {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
